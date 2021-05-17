89bbcbe950146ba31d776cf57608496a4413c941
ayoub-z <60653320+ayoub-z@users.noreply.github.com>
2021-05-17 15:09:11 +0200

    Initialized second machine inside main

--- src/com/Ayoub/Main.java
+++ src/com/Ayoub/Main.java
@@ -6,10 +6,19 @@ public class Main {
 
     public static void main(String[] args) {
         ArrayList<String> paths = new ArrayList<>();
+        ArrayList<String> states = new ArrayList<>();
+
+        // machine 1
         Node s0 = new Node("S0");
         Node s1 = new Node("S1");
         Node s2 = new Node("S2");
         Node s3 = new Node("S3");
+
+        // machine 2
+        Node running = new Node("R");
+        Node walking = new Node("W");
+        Node sitting = new Node("S");
+        Node jumping = new Node("J");
 
         s0.con.put('B',s1);
         s0.con.put('A',s2);
@@ -20,5 +29,15 @@ public class Main {
         s3.con.put('B',s0);
 
         s0.nodeRoute("ABABBABBAXABB", paths);
+
+        running.con.put('1',jumping);
+        running.con.put('2',walking);
+        walking.con.put('1',running);
+        walking.con.put('2',sitting);
+        sitting.con.put('1',walking);
+        jumping.con.put('1',sitting);
+        jumping.con.put('2',running);
+
+        sitting.nodeRoute("1212125", states);
     }
 }
