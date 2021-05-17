0008c96c2703acb294e8e57719743eea812858b0
ayoub-z <60653320+ayoub-z@users.noreply.github.com>
2021-05-17 11:46:18 +0200

    Basic node structure

---
+++ src/com/Ayoub/Main.java
@@ -0,0 +1,16 @@
+package com.Ayoub;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<String> paths = new ArrayList<>();
+        Node s0 = new Node("S0");
+        Node s1 = new Node("S1");
+        Node s2 = new Node("S2");
+        Node s3 = new Node("S3");
+
+
+    }
+}
